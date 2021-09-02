19bef054d56392eacb58b9cf39aab64550f24002
cristianhd <cristian.hd1996@gmail.com>
2021-09-02 19:33:18 -0300

    start NavBar component

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import logo from "../assets/logo-la-nacion.svg";
+import "../Style/NavBar.scss"
 
 function NavBar(props) {
     return (
         <div className="navbar">
-            NavBar
+            <nav>
+                <div>
+                    <img src={logo} alt="logo.svg"></img>
+                </div>
+                <div>
+                    
+                </div>
+            </nav>
         </div>
     );
 }
