91d9a6da642fb0c2774b3a7d9b46fe8a153ff479
cristianhd <cristian.hd1996@gmail.com>
2021-09-02 23:59:45 -0300

    SubNavBar Responsive

--- src/Components/SubNavBar.js
+++ src/Components/SubNavBar.js
@@ -2,27 +2,27 @@ import React from "react";
 import logoClub from "../assets/logo-club.svg";
 import DolarLabel from "./DolarLabel";
 import WeatherLabel from "./WeatherLabel";
+import '../Style/SubNavBar.scss'
 
 function SubNavBar(props) {
+ //Simulate Data API Dolar and Weather
   const dolarBna = { buy: "00,00", sell: "00,00" };
   const dolarBlue = { buy: "00,00", sell: "00,00" };
-  const geoWeather = {gradius: "00,0°", city:"Capital Federal"}
+  const geoWeather = { gradius: "00,0°", city: "Capital Federal" };
   return (
-    <div className="container">
-      <section className="sub-navbar">
-        <DolarLabel
-          bna={dolarBna}
-          blue={dolarBlue}
-        />
-        <WeatherLabel gradius={geoWeather.gradius} city={geoWeather.city}/>
 
-        <a href="/newsletters">Recibí Newsletters</a>
-        <a href="/clubnacion">
+      <section className="sub-navbar">
+    <div className="container">
+        <DolarLabel bna={dolarBna} blue={dolarBlue} />
+        <WeatherLabel gradius={geoWeather.gradius} city={geoWeather.city} />
+
+        <a className='newsl' href="/newsletters">Recibí Newsletters</a>
+        <a className='clubn' href="/clubnacion">
           <img src={logoClub} alt="logo-club.svg"></img>
           Descubrí tus beneficios
         </a>
-      </section>
     </div>
+      </section>
   );
 }
 
--- src/Components/WeatherLabel.js
+++ src/Components/WeatherLabel.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import weatherIcon from '../assets/weth-icon.svg'
 
 function WeatherLabel({gradius,city}) {
     return (
         <div className='weather'>
+        <img className='weth-icon'src={weatherIcon} alt='weth-icon.svg'></img>
         <label className='grad'>{gradius}</label>
         <label className='name'>{city}</label>
     </div>
