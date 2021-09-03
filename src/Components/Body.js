f9d3d023583af1f43973d9cf820349b931c61a69
cristianhd <cristian.hd1996@gmail.com>
2021-09-03 11:37:23 -0300

    render first mobile 3 news

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import CardArticle from './CardArticle';
+import '../Style/Body.scss'
 
 function Body({article}) {
     return (
         <div>
+            <section className='art-render'>
+            <CardArticle article={article} render={1}/>
+            <CardArticle article={article} render={2}/>
             <CardArticle article={article} render={3}/>
+
+            </section>
         </div>
     );
 }
