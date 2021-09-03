1cd82095db74e6f1a0c0e4ec73394876f547278e
cristianhd <cristian.hd1996@gmail.com>
2021-09-03 11:37:39 -0300

    prettier Body

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,18 +1,17 @@
-import React from 'react';
-import CardArticle from './CardArticle';
-import '../Style/Body.scss'
+import React from "react";
+import CardArticle from "./CardArticle";
+import "../Style/Body.scss";
 
-function Body({article}) {
-    return (
-        <div>
-            <section className='art-render'>
-            <CardArticle article={article} render={1}/>
-            <CardArticle article={article} render={2}/>
-            <CardArticle article={article} render={3}/>
-
-            </section>
-        </div>
-    );
+function Body({ article }) {
+  return (
+    <div>
+      <section className="art-render">
+        <CardArticle article={article} render={1} />
+        <CardArticle article={article} render={2} />
+        <CardArticle article={article} render={3} />
+      </section>
+    </div>
+  );
 }
 
 export default Body;
