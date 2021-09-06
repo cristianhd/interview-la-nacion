ebcd3c2f77ebeb99dac6c2dfe773db7efa146b98
cristianhd <cristian.hd1996@gmail.com>
2021-09-05 23:06:27 -0300

    create cabezal and megalateral

---
+++ src/Components/Cabezal.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../Style/Cabezal.scss'
+
+function Cabezal(props) {
+    return (
+        <section className='cabezal'>
+        
+
+
+        <div className='container'>
+
+
+        
+
+        </div>
+        </section>
+    );
+}
+
+export default Cabezal;
