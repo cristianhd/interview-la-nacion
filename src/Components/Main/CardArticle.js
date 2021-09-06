6e5695bf9e7e01eea5ffc0555e71058e36d68616
cristianhd <cristian.hd1996@gmail.com>
2021-09-06 17:03:16 -0300

    style width > 768px focal and card article

--- src/Components/Main/CardArticle.js
+++ src/Components/Main/CardArticle.js
@@ -2,28 +2,28 @@ import React from "react";
 import Article from "./Article";
 
 function CardArticle({ article, render }) {
- console.log(render);
+ 
   const { lead, title, subheader, img, marquee, autor, imgautor, url } =
     article;
 
   switch (render) {
     case 1:
       return (
-        <article>
+        <article className='article'>
           <Article elements={{ img, lead, title, marquee, url }} />
         </article>
       );
 
     case 2:
       return (
-        <article>
+        <article className='article'>
           <Article elements={{ lead, title, subheader, marquee, url }} />
         </article>
       );
 
     case 3:
       return (
-        <article>
+        <article className='article'>
           <Article elements={{ lead, title, imgautor, autor, url }} />
         </article>
       );
--- src/Components/Main/FocalRight.js
+++ src/Components/Main/FocalRight.js
@@ -1,37 +1,40 @@
 import React from "react";
 import CardArticle from "../Main/CardArticle.js";
-import '../../Style/FocalRight.scss'
+import "../../Style/FocalRight.scss";
 
 function FocalRight({ articles, title, link, url }) {
   const BlockArticles = articles.slice(1, 4);
-  console.log(BlockArticles);
- 
-
+  const articleLeft = BlockArticles.map((item,index)=>{
+    return index ? <CardArticle key={index} article={item} render={index + 1} /> : <></>
+  })
+  
   return (
     <div className="focal">
-    <div className='modul-title'>
-
-      {title && link ? (
-        <a href={url}>
+      <div className="modul-title">
+        {title && link ? (
+          <a href={url}>
+            <h1>{title}</h1>
+          </a>
+        ) : (
           <h1>{title}</h1>
-        </a>
-      ) : (
-        <h1>{title}</h1>
-      )}
-    </div>
-      
-      <div className="block-art">
-        {BlockArticles &&
-          BlockArticles.map((item, index) => {
-            return (
-              <CardArticle
-                key={index}
-                article={item}
-                render={index+1}
-              />
-            );
-          })}
+        )}
       </div>
+
+        {BlockArticles &&
+        
+     <div className="block-art">
+        
+        <div className='art-left'>
+
+        {articleLeft}
+        </div>
+        <div className='art-right'>
+        <CardArticle article={BlockArticles[0]} render={1} />
+
+        </div>
+           
+      </div>
+      }
     </div>
   );
 }
