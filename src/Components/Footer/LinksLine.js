d558556c0a5173a84d21a6742e035703c2b3f53b
cristianhd <cristian.hd1996@gmail.com>
2021-09-07 04:39:03 -0300

    finish style components

--- src/Components/Footer/LinksLine.js
+++ src/Components/Footer/LinksLine.js
@@ -1,22 +1,20 @@
 import React from "react";
-import '../../Style/LinksLine.scss'
+import "../../Style/LinksLine.scss";
 
-function LinksLine({ links , name }) {
+function LinksLine({ links, name }) {
   const rowLinks = links.map((item, index) => {
     const { url, link } = item;
     return (
-     
-        <a key={index} href={url}>{link}</a>
-    
+      <a key={index} href={url}>
+        {link}
+      </a>
     );
   });
-  return <div className={name}>
-  <ul className='row-links'>
-
-  {rowLinks}
-  
-  </ul>
-  </div>;
+  return (
+    <div className={name}>
+      <ul className="row-links">{rowLinks}</ul>
+    </div>
+  );
 }
 
 export default LinksLine;
--- src/Components/NavBar/WeatherLabel.js
+++ src/Components/NavBar/WeatherLabel.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import weatherIcon from '../../assets/weth-icon.svg'
-import '../../Style/WeatherLabel.scss'
+import React from "react";
+import weatherIcon from "../../assets/weth-icon.svg";
+import "../../Style/WeatherLabel.scss";
+import LinkImg from "../Footer/LinkImg";
 
-function WeatherLabel({gradius,city}) {
-    return (
-        <div className='weather'>
-        <img className='weth-icon'src={weatherIcon} alt='weth-icon.svg'></img>
-        <span className='grad'>{gradius}</span>
-        <span className='name'>{city}</span>
+function WeatherLabel({ gradius, city }) {
+  return (
+    <div className="weather">
+      <LinkImg img={weatherIcon} link="/weaher" name="weth-icon.svg" />
+      <span className="grad">{gradius}</span>
+      <span className="name">{city}</span>
     </div>
-    );
+  );
 }
 
 export default WeatherLabel;
